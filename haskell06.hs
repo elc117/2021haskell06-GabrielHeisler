4c84ea8d5fd4bbe4a21dd01ecc01808539f55659
Gabriel Vinícius Heisler <61278049+GabrielHeisler@users.noreply.github.com>
2021-06-16 16:36:12 -0300

    Update haskell06.hs

--- haskell06.hs
+++ haskell06.hs
@@ -3,3 +3,37 @@
 ends :: [Int] -> [Int]
 ends lst = head lst : last lst : []
 
+deduzame :: [Integer] -> [Integer]
+deduzame [] = []
+deduzame (x:xs) = (2 * x) : deduzame (xs)
+
+deduzame2 :: [Integer] -> [Integer]
+deduzame2 [] = []
+deduzame2 (x:xs) = if x > 2
+  then x : deduzame2 xs 
+  else deduzame2 xs
+
+
+geraTabela :: Int -> [(Int,Int)]
+geraTabela 0 = []
+geraTabela n = (n, n^2) : geraTabela (n-1)
+
+contido :: Char -> String -> Bool
+contido _ "" = False
+contido ch (x:xs) = ch == x || contido ch xs
+
+translate :: [(Float, Float)] -> [(Float, Float)]
+translate [] = []
+translate ((x,y):xs) = (x+2, y+2) : translate (xs) 
+
+{-
+countLongs :: [String] -> Int
+countLongs [] = 0
+countLongs (x:xs) = ( (length x) > 5) + countLongs xs
+-}
+
+onlyLongs :: [String] -> [String]
+onlyLongs [] = []
+onlyLongs (x:xs)
+ | (length x)>5 = x : onlyLongs xs
+ | otherwise = onlyLongs xs
